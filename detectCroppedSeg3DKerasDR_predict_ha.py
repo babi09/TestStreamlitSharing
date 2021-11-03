1d96356bc3cc3157af0e959590e870b81dc36644
babi09 <93528062+babi09@users.noreply.github.com>
2021-11-03 12:30:55 +0000

    Update detectCroppedSeg3DKerasDR_predict_ha.py
    debugging

--- detectCroppedSeg3DKerasDR_predict_ha.py
+++ detectCroppedSeg3DKerasDR_predict_ha.py
@@ -47,12 +47,7 @@ def singlePatientDetection(pName, baseline, params, organTarget):
     # perform PCA to numPC 
     numPC = 5; #50
     pca = PCA(n_components=numPC);
-    vol4Dvecs=np.reshape(vol4D0, (vol4D0.shape[0]*vol4D0.shape[1]*vol4D0.shape[2], vol4D0.shape[3]));
-    PCs=pca.fit_transform(vol4Dvecs);
-    vol4Dpcs=np.reshape(PCs, (vol4D0.shape[0],vol4D0.shape[1],vol4D0.shape[2], numPC));
-
     
-    print('Step1')
    
     return 0
 
